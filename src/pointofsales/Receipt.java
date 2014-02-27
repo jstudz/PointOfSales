d08fef39130bf731e62da1204b768eeb992c96a0
Arthas <Arthas@Arthas-PC>
2014-02-26 19:32:41 -0600

    edited receipt

--- src/pointofsales/Receipt.java
+++ src/pointofsales/Receipt.java
@@ -1,16 +1,23 @@
 package pointofsales;
 
 public class Receipt {
-    private LineItem[] lineItem;
+    private LineItem lineItem;
     private double total = 0;
     private String storeName;
     private double tax;
     
     public double getTotal() {
+        
         return total;
     }
     
     public double getTax() {
         return total * .051;
+    }
+    
+    
+    public static void main(String[] args) {
+        LineItem li = new LineItem(new Product(45, "Hat", "556A", new DollarsOffDiscount(10, 45, 1)));
+        System.out.println(li.getSubTotal());
     }
 }
