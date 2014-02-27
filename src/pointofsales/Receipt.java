cf0f11eeef3779b1d6e7d8c78ce27751146d14ad
Arthas <Arthas@Arthas-PC>
2014-02-26 18:44:32 -0600

    Added some stuff to receipt

--- src/pointofsales/Receipt.java
+++ src/pointofsales/Receipt.java
@@ -2,5 +2,15 @@ package pointofsales;
 
 public class Receipt {
     private LineItem[] lineItem;
+    private double total = 0;
+    private String storeName;
+    private double tax;
     
+    public double getTotal() {
+        return total;
+    }
+    
+    public double getTax() {
+        return total * .051;
+    }
 }
